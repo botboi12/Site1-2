d97d3a479cd04416b35feda36fa87d84c540ead4
botboi12 <56870692+botboi12@users.noreply.github.com>
2021-07-30 21:19:11 +1000

    Initial commit

---
+++ template-parts/footer-styles.php
@@ -0,0 +1,39 @@
+<!-- footer styles -->
+
+<style>.u-footer {
+  background-image: none;
+}
+.u-footer .u-sheet-1 {
+  min-height: 120px;
+}
+.u-footer .u-text-1 {
+  width: 417px;
+  margin: 49px auto;
+}
+.u-footer .u-btn-1 {
+  background-image: none;
+  padding: 0;
+}
+@media (max-width: 1199px) {
+  .u-footer .u-sheet-1 {
+    min-height: 99px;
+  }
+}
+@media (max-width: 991px) {
+  .u-footer .u-sheet-1 {
+    min-height: 76px;
+  }
+}
+@media (max-width: 767px) {
+  .u-footer .u-sheet-1 {
+    min-height: 57px;
+  }
+}
+@media (max-width: 575px) {
+  .u-footer .u-sheet-1 {
+    min-height: 36px;
+  }
+  .u-footer .u-text-1 {
+    width: 340px;
+  }
+}</style>
